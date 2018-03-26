ed0a415898830d05a2e122da5f7441ed4670630d
Somer <907414556@qq.com>
2018-03-26 22:46:55 +0800

    add 全局跨域配置

---
+++ src/main/java/top/somer/kernel/configure/CorsConfigure.java
@@ -0,0 +1,29 @@
+package top.somer.kernel.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域全局配置
+ *
+ * @author Somer
+ * @date 2018-03-08 14:01
+ **/
+@Configuration
+public class CorsConfigure {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
