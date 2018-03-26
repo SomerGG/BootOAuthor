192ce5b3d12f7f5347a6e2e46072e3e8f2a532fe
Somer <907414556@qq.com>
2018-03-26 22:46:11 +0800

    add beetl模板引擎配置

---
+++ src/main/java/top/somer/kernel/configure/BeetlConfigure.java
@@ -0,0 +1,53 @@
+package top.somer.kernel.configure;
+
+import com.google.common.collect.Maps;
+import org.beetl.core.resource.ClasspathResourceLoader;
+import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
+import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
+
+import java.util.Map;
+
+@Configuration
+public class BeetlConfigure {
+
+    @Bean(initMethod = "init", name = "beetlConfig")
+    public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
+        BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
+        ResourcePatternResolver patternResolver = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader());
+        Map<String, Object> runtimeVars = Maps.newHashMap();
+
+        //获取Spring Boot 的ClassLoader;
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        if (loader == null) {
+            loader = BeetlConfigure.class.getClassLoader();
+        }
+        ClasspathResourceLoader classpathResourceLoader = new ClasspathResourceLoader(loader,
+                "templates");
+        beetlGroupUtilConfiguration.setResourceLoader(classpathResourceLoader);
+        beetlGroupUtilConfiguration.setSharedVars(runtimeVars);
+
+
+        beetlGroupUtilConfiguration.setConfigFileResource(patternResolver.getResource("classpath:beetl.yml"));
+        beetlGroupUtilConfiguration.init();
+        //如果使用了优化编译器，涉及到字节码操作，需要添加ClassLoader
+        beetlGroupUtilConfiguration.getGroupTemplate().setClassLoader(loader);
+        return beetlGroupUtilConfiguration;
+
+    }
+
+    @Bean(name = "beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(
+            @Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+        BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
+        beetlSpringViewResolver.setOrder(0);
+        beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
+        return beetlSpringViewResolver;
+    }
+}
