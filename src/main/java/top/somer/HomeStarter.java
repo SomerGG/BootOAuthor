779e802009093651696fedd1fd0b1d40c54ec8e7
Somer <907414556@qq.com>
2018-03-26 23:04:30 +0800

    add 启动类

---
+++ src/main/java/top/somer/HomeStarter.java
@@ -0,0 +1,12 @@
+package top.somer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeStarter.class, args);
+    }
+}
